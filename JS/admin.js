9a2427f33a74513af14dc27e60123fa2008bda0c
Chao Tzu Hsuan <410208044@gms.ndhu.edu.tw>
2024-12-07 21:36:06 +0800

    新增 取得訂單列表api並渲染

--- JS/admin.js
+++ JS/admin.js
@@ -1,3 +1,66 @@
+//客製化路徑
+const baseUrl = "https://livejs-api.hexschool.io";
+const apiPath = "emma";
+const adminApi = `${baseUrl}/api/livejs/v1/admin/${apiPath}`;
+const token = 'QXnvOqCYJ7afzlGmYhiG2HZBEOL2';
+
+//建立變數
+let orderData = [];
+const orderPageTableBody = document.querySelector('.orderPage-table tbody');
+
+function getOrder(){
+    axios.get(`${adminApi}/orders`,{
+        headers:{
+            authorization:token
+        }
+    }).then((res) => {
+        console.log(res)
+        orderData = res.data.orders;
+        renderOrder();
+    }).catch(err =>{
+        console.log(err)
+    })
+}
+
+//渲染到畫面上
+function renderOrder(){
+   let str = '';
+   orderData.forEach(order=>{
+    let productStr = "";
+    //訂單裡的數量也一樣跑fprEach，記得渲染到畫面上
+    order.products.forEach((product) =>{
+        productStr +=`<p>${product.title}</p> x ${product.quantity}`;
+    })
+    str += `<tr>
+            <td>${order.id}</td>
+            <td>
+              <p>${order.user.name}</p>
+              <p>${order.user.tel}</p>
+            </td>
+            <td>${order.user.address}</td>
+            <td>${order.user.email}</td>
+            <td>
+             ${productStr}
+            </td>
+            <td>${order.createdAt}</td>
+            <td class="orderStatus">
+              <a href="#">${order.paid ? "已處理" : "未處理"}</a>
+            </td>
+            <td>
+              <input type="button" class="delSingleOrder-Btn" value="刪除">
+            </td>
+          </tr>`
+   })
+   orderPageTableBody.innerHTML = str;
+}
+
+
+
+ getOrder()
+
+
+
+
 // C3.js
 let chart = c3.generate({
     bindto: '#chart', // HTML 元素綁定
